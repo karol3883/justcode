a67c89ebdf95ebf07f621d3b8a77a0519ec1c0f5
karol.abramczyk <karol.abramczyk@britenet.com.pl>
2023-01-09 00:18:09 +0100

    Test

--- src/Controller/Admin/NotificationsController.php
+++ src/Controller/Admin/NotificationsController.php
@@ -22,13 +22,33 @@ class NotificationsController extends AbstractController
         );
     }
 
-    #[Route('/{slug}', name: 'app_admin_notification_details')]
+    #[Route('/id/{slug}', name: 'app_admin_notification_details')]
     public function notificationDetails(NotificationCounterRepository $notificationCounterRepository): Response
     {
         return $this->render('admin/notifications/index.html.twig',
             [
                 'controller_name' => 'TodoController',
                 'notification_counters' => $notificationCounterRepository->getNotificationsFromMonth(),
+            ]
+        );
+    }
+
+
+    #[Route('/create', name: 'app_admin_notification_create')]
+    public function createNotification(): Response
+    {
+        return $this->render('admin/notifications/create.html.twig',
+            [
+                'controller_name' => 'TodoController',
+            ]
+        );
+    }
+    #[Route('/test', name: 'chuj')]
+    public function chuj(): Response
+    {
+        return $this->render('admin/notifications/create.html.twig',
+            [
+                'controller_name' => 'TodoController',
             ]
         );
     }
